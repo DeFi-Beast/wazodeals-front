41b5066904448fb014db34f2a5f270fc873ae0ba
ALIENWARCLUBNFT <princzny@gmail.com>
2022-01-26 10:18:46 +0100

    Add baseURL in user page

--- src/Pages/User/index.js
+++ src/Pages/User/index.js
@@ -36,13 +36,35 @@ const User = () => {
   //   getUser();
   // });
    
-  // setEmail(localStorage.getItem("email"));
-  // setToken(localStorage.getItem("token"));
-  // setUserId(localStorage.getItem("userId"));
+  
  
   // 827e656f-42db-445a-8c03-41bea37b393e
-  const getUser = () => {
-    fetch(`${BaseURL}/user/827e656f-42db-445a-8c03-41bea37b393e`).then((response) => {
+  // const getUser = () => {
+  //   axios.get(`${BaseURL}/user/827e656f-42db-445a-8c03-41bea37b393e`).then((response) => {
+  //     const result = response.data;
+  //     console.log(`${BaseURL}/user/${localStorage.getItem("userId")}`)
+  //     if (result.success) {
+  //       console.log(result.user)
+  //        setCode(result.user.referralCode)
+  //     }
+  //   });
+  // }
+  
+  setEmail(localStorage.getItem("email"));
+  setToken(localStorage.getItem("token"));
+  setUserId(localStorage.getItem("userId"));
+
+  useEffect(() => {
+
+    setEmail(localStorage.getItem("email"));
+  setToken(localStorage.getItem("token"));
+  setUserId(localStorage.getItem("userId"));
+    
+    console.log(`${userId}`)
+    console.log(`${BaseURL}/user/${userId}`)
+
+    
+    axios.get(`${BaseURL}/user/${userId}`).then((response) => {
       const result = response.data;
       console.log(`${BaseURL}/user/${localStorage.getItem("userId")}`)
       if (result.success) {
@@ -50,49 +72,43 @@ const User = () => {
          setCode(result.user.referralCode)
       }
     });
-  }
-  getUser()
-//   useEffect(() => {
-
-    
-//     console.log(`${userId}`)
-//     console.log(`${BaseURL}/user/${userId}`)
+  
     
     
-//     axios.get(`${BaseURL}/user`).then((response) => {
-//       const result = response.data;
-//     //   console.log(result);
-//       if (result.success) {
-//         // console.log("user");
-//         // console.log(result.users);
+    axios.get(`${BaseURL}/user`).then((response) => {
+      const result = response.data;
+    //   console.log(result);
+      if (result.success) {
+        // console.log("user");
+        // console.log(result.users);
        
-//       }
-//     });
-//     const config = {
-//       headers: {
-//           "Content-type": "application/json",
-//            "Authorization": `Bearer ${localStorage.getItem("token")}`,
-//       },
-//  };
-//     axios.get(`${BaseURL}/referred`, config).then((response) => {
-//       const result = response.data;
-//     //   console.log(result);
-//       if (result.success) {
-//         let accounts = []
-//         result.accounts.map(account => {
-//           if(account.active){
-//             return accounts.push(account)
+      }
+    });
+    const config = {
+      headers: {
+          "Content-type": "application/json",
+           "Authorization": `Bearer ${localStorage.getItem("token")}`,
+      },
+ };
+    axios.get(`${BaseURL}/referred`, config).then((response) => {
+      const result = response.data;
+    //   console.log(result);
+      if (result.success) {
+        let accounts = []
+        result.accounts.map(account => {
+          if(account.active){
+            return accounts.push(account)
             
-//           }
-//           else console.log("not referred")
-//         })
-//         console.log(accounts);
-//         console.log(result.accounts);
-//         setPoint(100 + (accounts.length * 30))
-//         setReferred(accounts.length)
-//       }
-//     });
-//   }, []);
+          }
+          else console.log("not referred")
+        })
+        console.log(accounts);
+        console.log(result.accounts);
+        setPoint(100 + (accounts.length * 30))
+        setReferred(accounts.length)
+      }
+    });
+  }, []);
 
   return (
     <LayoutDefault>
