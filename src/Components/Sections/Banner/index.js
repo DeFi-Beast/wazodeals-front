bf68dd7760f4bed71c8de7245a9332ec2b4a9e64
ALIENWARCLUBNFT <princzny@gmail.com>
2022-02-02 21:35:40 +0100

    update wazopoints

--- src/Components/Sections/Banner/index.js
+++ src/Components/Sections/Banner/index.js
@@ -9,10 +9,10 @@ const Banner = ()  => {
         <Row>
             <Div>
                 <h2>
-                SIGN UP NOW &amp; EARN 500 POINTS
+                SIGN UP NOW &amp; EARN 1000 POINTS
                 </h2>
                 <p className={Classes.Content}>
-                Refer &amp; earn 200 points per referral
+                Refer &amp; earn 500 points per referral
                 </p>
                 <Button as="a" href="/register" >SIGN UP NOW</Button>
             </Div>
